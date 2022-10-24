26f32c3f3deab863ac95f7ed51b83de2cb5bace2
Morty <2209511776@qq.com>
2022-10-24 09:35:43 +0800

    seeond commit

--- src/main/java/com/atgit/GitTest4.java
+++ src/main/java/com/atgit/GitTest4.java
@@ -3,5 +3,6 @@ package com.atgit;
 public class GitTest4 {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
